9a339f8643f0ce9aa93323f32299a75e1b42c65a
gabrielSabato <88863473+gabrielSabato@users.noreply.github.com>
2021-08-26 20:17:54 -0300

    Update main.c

--- exe01/main.c
+++ exe01/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main() {
+   //DECLARACAO DAS VARIAVEIS
    int n1, n2;
+   //SOLICITA AO USUARIO QUE INFORME OS VALORES
+   printf("Faz a soma de dois numeros:\n");
+   printf("Digite os dois valores.\n");
+   //OBTEM OS VALORES
    scanf("%i %i", &n1, &n2);
+   //IMPRIME OS VALORES SOMADOS.
    printf("SOMA = %i\n", n1+n2);
    return 0;
 }
